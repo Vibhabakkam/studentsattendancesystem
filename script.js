f453fa85d5eaca7e7fc579cf07ea451a92b03a5e
Vibhabakkam <vibhabakkam22@gmail.com>
2023-02-26 12:54:56 +0530

    comment

--- script.js
+++ script.js
@@ -1,114 +1,143 @@
-function addstudent(){
+function addstudent() {
     alert("working")
     var studentName = document.getElementById("addingstudents").value;
-    console.log(studentName,"studentName" )
+    console.log(studentName, "studentName")
 
     var StudentList = JSON.parse(localStorage.getItem("StudentList")) || [];
 
-    StudentList.push({nameofstudent : studentName, attendance : []} )
+    StudentList.push({ nameofstudent: studentName, attendance: [] })
 
     localStorage.setItem("StudentList", JSON.stringify(StudentList));
-    document.getElementById("addingstudents").value ="";
+    document.getElementById("addingstudents").value = "";
     markingAttendance();
     displayStudentlist();
-} 
+}
 
-function displayStudentlist(){
-    //taking data from storage
-    var StudentList = JSON.parse(localStorage.getItem('StudentList'))
-    console.log(StudentList,"StudentList")
+// function displayStudentlist(){
+//     //taking data from storage
+//     var StudentList = JSON.parse(localStorage.getItem('StudentList'))
+//     console.log(StudentList,"StudentList")
 
-    //taking tags from html
-    var divFromHTML = document.getElementById("displayStudentlist")
-    console.log(divFromHTML,"divFromHTML")
+//     //taking tags from html
+//     var divFromHTML = document.getElementById("displayStudentlist")
+//     console.log(divFromHTML,"divFromHTML")
 
-    //looping  over userlist to create perfect structure
-    var students = [];
-    for (var i = 0; i< StudentList.length;i++){
-        students += `<div><p>${StudentList[i].nameofstudent}</p>
-        </div>`
-    }
+//     //looping  over userlist to create perfect structure
+//     var students = [];
+//     for (var i = 0; i< StudentList.length;i++){
+//         students += `<div><p>${StudentList[i].nameofstudent}</p>
+//         </div>`
+//     }
 
-    console.log(students,"students")
-    divFromHTML.innerHTML = students;
-} 
-displayStudentlist()
+//     console.log(students,"students")
+//     divFromHTML.innerHTML = students;
+// } 
+// displayStudentlist()
 
-function getRealTime(){
+function getRealTime() {
     var dateAndTime = new Date();
-    var date = dateAndTime.toJSON().slice(0,10);
+    var date = dateAndTime.toJSON().slice(0, 10);
     var addDate = document.getElementById("datehere");
     addDate.innerText = date;
 }
 getRealTime();
 
-function markingAttendance(){
+function markingAttendance() {
     var StudentList = JSON.parse(localStorage.getItem('StudentList'))
-    console.log(StudentList,"StudentList")
+    console.log(StudentList, "StudentList")
 
     var divFromHTML = document.getElementById("markingAttendance")
-    console.log(divFromHTML,"divFromHTML")
+    console.log(divFromHTML, "divFromHTML")
 
     var students = [];
-    for (var i = 0; i< StudentList.length;i++){
+    for (var i = 0; i < StudentList.length; i++) {
         students += `<div id="presentdiv" ><p>${StudentList[i].nameofstudent}</p><p><i onclick="present(${i})" class="fa-solid fa-check"></i></p><p><i onclick="absent(${i})" class="fa-solid fa-xmark"></i></p></div>`
     }
 
-    console.log(students,"students")
+    console.log(students, "students")
     divFromHTML.innerHTML = students;
-} 
+}
 markingAttendance();
-function present(index){
-   console.log("index",index)
-   var dateAndTime = new Date();
-    var date = dateAndTime.toJSON().slice(0,10);
-    console.log(date,"date here")
+function present(index) {
+    console.log("index", index)
+    var dateAndTime = new Date();
+    var date = dateAndTime.toJSON().slice(0, 10);
+    console.log(date, "date here")
 
-     var StudentListFromLS = JSON.parse(localStorage.getItem("StudentList"))
-     var user = StudentListFromLS[index];
-     var flage = false;
-     for (var i=0; i< user.attendance.length; i++){
+    var StudentListFromLS = JSON.parse(localStorage.getItem("StudentList"))
+    var user = StudentListFromLS[index];
+    var flage = false;
+    for (var i = 0; i < user.attendance.length; i++) {
         if (!!user.attendance[i][date]) {
             flage = true;
         }
-     }
+    }
 
-     if (flage === false){
+    if (flage === false) {
         var obj = {};
-        obj [date] = "present"
+        obj[date] = "present"
         user.attendance.push(obj)
-        localStorage.setItem("StudentList",JSON.stringify(StudentListFromLS))
-     }
+        localStorage.setItem("StudentList", JSON.stringify(StudentListFromLS))
+    }
 
-     else {
+    else {
         alert('Already Marked')
-     }
-    
+    }
+
 }
 
-function absent(index){
+function absent(index) {
     // alert("working on absent")
     var dateAndTime = new Date();
-     var date = dateAndTime.toJSON().slice(0,10);
-     console.log(date,"date here")
+    var date = dateAndTime.toJSON().slice(0, 10);
+    console.log(date, "date here")
 
-     var StudentListFromLS = JSON.parse(localStorage.getItem("StudentList"))
-     var user = StudentListFromLS[index];
-     var flage = false;
-     for (var i=0; i< user.attendance.length; i++){
+    var StudentListFromLS = JSON.parse(localStorage.getItem("StudentList"))
+    var user = StudentListFromLS[index];
+    var flage = false;
+    for (var i = 0; i < user.attendance.length; i++) {
         if (!!user.attendance[i][date]) {
             flage = true;
         }
-     }
+    }
 
-     if (flage === false){
+    if (flage === false) {
         var obj = {};
-        obj [date] = "absent"
+        obj[date] = "absent"
         user.attendance.push(obj)
-        localStorage.setItem("StudentList",JSON.stringify(StudentListFromLS))
-     }
+        localStorage.setItem("StudentList", JSON.stringify(StudentListFromLS))
+    }
 
-     else {
+    else {
         alert('Already Marked')
-     }
- }
+    }
+}
+
+function displayingStudentNamelist() {
+    var idFromHTML = document.getElementById("studentName")
+    var dataFromLS = JSON.parse(localStorage.getItem("StudentList"))
+    var nameArray = [];
+    for (var i = 0; i < dataFromLS.length; i++) {
+        nameArray += `<div>${dataFromLS[i].nameOfStudent}</div>`
+    }
+    idFromHTML.innerHTML = nameArray;
+}
+displayingStudentNamelist()
+
+function displayingStudentsattendance(){
+    var idFromHTML = document.getElementById("studentsAttendance");
+    var dataFromLS = JSON.parse(localStorage.getItem("StudentList"));
+    console.log(dataFromLS,"dataFromLS")
+    var dates = [];
+    for  (var k = 0; k < dataFromLS.length; k++  ){
+        for (var l = 0; l < dataFromLS[k].attendance.length; l++){
+            if (!dates.includes(Object.keys(dataFromLS[k].attendance[l]))[0]){
+            dates.push((Object.keys(dataFromLS[k].attendance[l]))[0])
+            }
+
+        }
+  
+    }
+    console.log(dates,"dates here")
+}
+displayingStudentsattendance()
